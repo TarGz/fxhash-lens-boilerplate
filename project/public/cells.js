e7e52859c10d2b3074b26ecd041cd4c595d9cf21
Julien Terraz <julien@terraz.org>
2024-01-17 15:36:11 +0100

    Cleanning and documenting using Cursor.sh

--- project/public/cells.js
+++ project/public/cells.js
@@ -1,7 +1,12 @@
 class Cell {
+	// The Cell class represents a cell in the grid.
+	// Each cell has properties to store its state, position, and possible solutions.
+	// It also has methods to get its neighbours and calculate its entropy.
+
 	// Value 	list of tile ID 1,2,3,4,...
 	constructor(x, y) {
-		// console.log("Cell(value,x,y)",x);
+		// The constructor initializes the cell with the given x and y coordinates.
+		// It also sets the initial state of the cell to uncollapsed and not empty.
 		this.collapsed = false;
 		this.empty = false;
 		this.tile = undefined;
@@ -22,6 +27,8 @@ class Cell {
 
 
 	getNeighboursCell(x, y) {
+		// This method returns the neighbouring cell at the given x and y coordinates.
+		// If the coordinates are out of bounds, it returns an empty cell.
 		var emptyCell = new Cell(x, y);
 		emptyCell.tile = empty_tile;
 		emptyCell.collapsed = true;
@@ -43,13 +50,12 @@ class Cell {
 	}
 
 
-	// logCondition(cx,cy,text){
-	// 	if(this.x==cx && this.y == cy){
-	// 		console.log("lc",x,y,text);
-	// 	}
-	// }	
-
 	calculateEntropy(tiles) {
+		// This method calculates the entropy of the cell.
+		// It checks the possible tiles that can be placed in the cell based on the connectors of the neighbouring cells.
+		// It then stores the possible solutions in the respective direction arrays (north, est, south, west).
+		// Finally, it selects a tile from the possible solutions based on the number of connectors and collapses the cell.
+
 		if (this.y % 2 != 0) {
 			this.decal = true;
 		}
@@ -57,141 +63,128 @@ class Cell {
 
 		var logx = 10;
 		var logy = 20;
+		// Check if the cell is not collapsed
 		if (!this.collapsed) {
+			// Iterate over all tiles
 			for (var i = 0; i < tiles.length; i++) {
 				var T = tiles[i];
 
-				// CHECK BETWEEN OPTIONS OF THE NORTH CELL AND THE TILE DATABASE
+				// Check between options of the north cell and the tile database
 
 				// NORTH
 				var north_neighbours_cell;
+				// If the cell is decal, get the north neighbour cell at x+1, y-1
 				if (this.decal) {
 					north_neighbours_cell = this.getNeighboursCell(this.x + 1, this.y - 1);
 				} else {
+					// Else get the north neighbour cell at x, y-1
 					north_neighbours_cell = this.getNeighboursCell(this.x, this.y - 1);
 				}
 
-
+				// If the north neighbour cell is collapsed and has a tile
 				if (north_neighbours_cell.collapsed && north_neighbours_cell.tile) {
+					// If the connector of the north neighbour cell matches with the tile connector
 					if (north_neighbours_cell.tile.connector[2] == T.connector[0]) {
+						// Push the tile id to the north array and set n to true
 						this.north.push(T.id);
 						this.n = true;
-						// if(this.x == logx && this.y == logy ) console.log("tile connectors",T.connector[0]);
-						// if(this.x == logx && this.y == logy ) console.log("neighbours north connectors",north_neighbours_cell.tile.connector[2]);
-						// if(this.x == logx && this.y == logy ) console.log("NNC",this.x,":",this.y-1,"T.id->",T.id);
 					}
 				}
 
 				// EST
 
 				var est_neighbours_cell;
+				// If the cell is decal, get the est neighbour cell at x+1, y+1
 				if (this.decal) {
 					est_neighbours_cell = this.getNeighboursCell(this.x + 1, this.y + 1);
 				} else {
+					// Else get the est neighbour cell at x, y+1
 					est_neighbours_cell = this.getNeighboursCell(this.x, this.y + 1);
 				}
 
+				// If the est neighbour cell is collapsed and has a tile
 				if (est_neighbours_cell.collapsed && est_neighbours_cell.tile) {
+					// If the connector of the est neighbour cell matches with the tile connector
 					if (est_neighbours_cell.tile.connector[3] == T.connector[1]) {
+						// Push the tile id to the est array and set e to true
 						this.est.push(T.id);
 						this.e = true;
-						if (this.x == 0 && this.y == logy) ("EEE", this.x - 1, "-", this.y, "->", T.id);
 					}
 				}
 
 				// SOUTH
 				var souht_neighbours_cell;
+				// If the cell is decal, get the south neighbour cell at x, y+1
 				if (this.decal) {
 					souht_neighbours_cell = this.getNeighboursCell(this.x, this.y + 1);
 				} else {
+					// Else get the south neighbour cell at x-1, y+1
 					souht_neighbours_cell = this.getNeighboursCell(this.x - 1, this.y + 1);
 				}
 
-
-				// console.log("souht_neighbours_cell",souht_neighbours_cell);
+				// If the south neighbour cell is collapsed and has a tile
 				if (souht_neighbours_cell.collapsed && souht_neighbours_cell.tile) {
+					// If the connector of the south neighbour cell matches with the tile connector
 					if (souht_neighbours_cell.tile.connector[0] == T.connector[2]) {
+						// Push the tile id to the south array and set s to true
 						this.south.push(T.id);
 						this.s = true;
-						if (this.x == 0 && this.y == 0) ("SSS", this.x, "-", this.y + 1, "->", T.id);
 					}
 				}
 
 				// WEST
 				var west_neighbours_cell;
+				// If the cell is decal, get the west neighbour cell at x, y-1
 				if (this.decal) {
 					west_neighbours_cell = this.getNeighboursCell(this.x, this.y - 1);
 				} else {
+					// Else get the west neighbour cell at x-1, y-1
 					west_neighbours_cell = this.getNeighboursCell(this.x - 1, this.y - 1);
 				}
 
+				// If the west neighbour cell is collapsed and has a tile
 				if (west_neighbours_cell.collapsed && west_neighbours_cell.tile) {
+					// If the connector of the west neighbour cell matches with the tile connector
 					if (west_neighbours_cell.tile.connector[1] == T.connector[3]) {
+						// Push the tile id to the west array and set w to true
 						this.west.push(T.id);
 						this.w = true;
-						if (this.x == 0 && this.y == 0) ("WWW", this.x + 1, "-", this.y, "->", T.id);
 					}
-				}
-
-				if (this.x == logx && this.y == logy) {
-					if (this.x == logx && this.y == logy) console.log("current cell", this);
-					// console.log("this.decal",this.decal);
-					// console.log("north_neighbours_cell",north_neighbours_cell);
-					// console.log("est_neighbours_cell",est_neighbours_cell);
-					// console.log("souht_neighbours_cell",souht_neighbours_cell);
-					// console.log("west_neighbours_cell",west_neighbours_cell);
 				}
 
 			}
 		}
 
-
+		// If n, e, s, w are true, push the respective arrays to the tempsolutions array
 		if (this.n) this.tempsolutions.push(this.north);
 		if (this.e) this.tempsolutions.push(this.est);
 		if (this.s) this.tempsolutions.push(this.south);
 		if (this.w) this.tempsolutions.push(this.west);
 
-		// console.log("TEMP SOLUTION",this.x,this.y,this.tempsolutions);
-		// this.solutions = this.tempsolutions.reduce((acc, curVal) => acc.concat(curVal), []);
-		// console.log("SOLUTION",this.solutions);
-		// var set = new Set(this.solutions);
-		// console.log("commonElements",commonElements);
-
-
+		// Get the count of directions
 		var directionCount = this.tempsolutions.length;
 
-		// console.log(">",this.x,"-",this.y, " directionCount",directionCount);
+		// If there are no directions, return
 		if (directionCount == 0) {
-			// console.log("no solution, skip");
 			return;
 		}
+		// If there is only one direction, get the solution by reducing the tempsolutions array and collapsing the cell
 		if (directionCount == 1) {
-			// debugger;
 			var sol = this.tempsolutions.reduce((acc, curVal) => acc.concat(curVal), []);
-			// console.log("1 direction sol ->"+sol);
-			this.tile = tiles_database[getRandomTile(sol, fxrand())];
-			// this.tile = tiles_database[getMostConnectedSolution(sol)];
 			this.tile = tiles_database[getRandomSolutionByConnectorCount(sol, connector_count_favor)];
 			this.collapsed = true;
 		}
 
-
+		// If there are two directions, get the solution by filtering the tempsolutions array and collapsing the cell
 		if (directionCount == 2) {
 			var sol = this.tempsolutions[0].filter((element) => {
 				return this.tempsolutions[1].includes(element);
 			});
-			// console.log("2 direction sol ->",sol);
-
-
-			// this.tile = tiles_database[random(sol)];
-			// this.tile = tiles_database[getRandomTile(sol, fxrand())];
-			// this.tile = tiles_database[getMostConnectedSolution(sol)];
 			this.tile = tiles_database[getRandomSolutionByConnectorCount(sol, connector_count_favor)];
 			this.collapsed = true;
-			// return;
 		}
 
-
+		// If there are three directions, get the solution by filtering the tempsolutions array twice and collapsing the cell
 		if (directionCount == 3) {
 			var sol1 = this.tempsolutions[0].filter((element) => {
 				return this.tempsolutions[1].includes(element);
@@ -199,22 +192,12 @@ class Cell {
 			var sol = this.tempsolutions[2].filter((element) => {
 				return sol1.includes(element);
 			});
-			// console.log("3 direction sol ->",sol);
-
-			// getMostConnectedSolution(sol);
-			// this.tile = tiles_database[random(sol)];
-			// this.tile = tiles_database[getRandomTile(sol, fxrand())];
-			// this.tile = tiles_database[getMostConnectedSolution(sol)];
 			this.tile = tiles_database[getRandomSolutionByConnectorCount(sol, connector_count_favor)];
-
 			this.collapsed = true;
-			// return;
 		}
 
-
+		// If there are four directions, get the solution by filtering the tempsolutions array three times and collapsing the cell
 		if (directionCount == 4) {
-			// console.log("4 direction sol ->",sol);
-			// return;
 			var sol1 = this.tempsolutions[0].filter((element) => {
 				return this.tempsolutions[1].includes(element);
 			});
@@ -224,18 +207,9 @@ class Cell {
 			var sol = sol1.filter((element) => {
 				return sol2.includes(element);
 			});
-
-			// console.log("4 direction sol ->",sol);
-
-			// this.tile = tiles_database[random(sol)];
-			// this.tile = tiles_database[getRandomTile(sol, fxrand())];
-			// this.tile = tiles_database[getMostConnectedSolution(sol)];
 			this.tile = tiles_database[getRandomSolutionByConnectorCount(sol, connector_count_favor)];
 			this.collapsed = true;
-			// return;
 		}
-		// debugger;
-
 	}
 
 }
--- project/public/layer.js
+++ project/public/layer.js
@@ -1,69 +1,81 @@
 class Layer {
-	// Value 	list of tile ID 1,2,3,4,...
-	constructor(id,col,angle,h_flip,v_flip) {
-                
-                // console.log("layer N°",id);
-                // console.log(color);
-                // var new_angle;
-                // if(h_flip) new_angle = 180-angle;
-                // if(v_flip) new_angle = 180-angle;
-                // if(!v_flip&&!h_flip) new_angle = angle;
+    /**
+     * Represents a Layer.
+     * @constructor
+     * @param {number} id - The id of the layer.
+     * @param {Array} col - The color of the layer.
+     * @param {number} angle - The angle of the layer.
+     * @param {boolean} h_flip - The horizontal flip status of the layer.
+     * @param {boolean} v_flip - The vertical flip status of the layer.
+     */
+    constructor(id,col,angle,h_flip,v_flip) {
+        this.id = id;
+        this.brush_angle=angle;
+        this.fliped_brush_angle=angle;
+        this.fixed_brush_angle=angle;
+        this.needupdate = false;
+        this.horizontal_flip = h_flip; 
+        this.vertical_flip = v_flip; 
+        this.color = col[1];
+        this.color_name = col[0];
+        this.vector_canvas;
 
-
-                this.id = id;
-                this.brush_angle=angle;
-                this.fliped_brush_angle=angle;
-                this.fixed_brush_angle=angle;
-                this.needupdate = false;
-                this.horizontal_flip = h_flip; 
-                this.vertical_flip = v_flip; 
-                this.color = col[1];
-                this.color_name = col[0];
-                // console.log("color_name",this.color_name);
-                // console.log("color",this.color);
-                this.vector_canvas;
-
-                console.log("LayerObj:constructor:"+this.id,"-",this.color_name,"h_flip:"+h_flip,"v_flip:"+v_flip); 
-                console.log("LayerObj:request angle->",this.brush_angle);
-                if(h_flip && !v_flip){
-                        this.flip_brush_angle();
-                }else if(v_flip &! h_flip){
-                        this.flip_brush_angle();
-                }
-                this.fix_angle();
-
-	}
-        flip_brush_angle(){
-                console.log("LayerObj:FLIP()");
-                // this.horizontal_flip = true;
-                if(this.brush_angle == 45 ) this.fliped_brush_angle = 135;
-                if(this.brush_angle == 135 ) this.fliped_brush_angle = 45;
-                console.log("LayerObj:flip_brush_angle->",this.fliped_brush_angle);
+        if(h_flip && !v_flip){
+            this.flip_brush_angle();
+        }else if(v_flip &! h_flip){
+            this.flip_brush_angle();
         }
+        this.fix_angle();
+    }
 
-        fix_angle(){
-                // Comme o ntoune de 45° elms tiles on dois faire "tourner els angles de 45°"
-                // console.log("LayerObj:fix_angle()");
-                // console.log("LayerObj:color_name->",this.color_name);
-                console.log("LayerObj:fliped_brush_angle->",this.fliped_brush_angle);
-                if(this.fliped_brush_angle == 0 ) this.fixed_brush_angle = 45;
-                if(this.fliped_brush_angle == 45 ) this.fixed_brush_angle = 0;
-                if(this.fliped_brush_angle ==90 ) this.fixed_brush_angle = 135;
-                if(this.fliped_brush_angle ==135 ) this.fixed_brush_angle = 90;
-                console.log("LayerObj:fixed brush_angle->",this.fixed_brush_angle);
-  
-                
+    /**
+     * Flips the brush angle of the layer.
+     */
+    flip_brush_angle(){
+        this.fliped_brush_angle = (this.brush_angle == 45) ? 135 : 45;
+    }
+
+    /**
+     * Fixes the brush angle of the layer.
+     */
+    fix_angle(){
+        switch(this.fliped_brush_angle) {
+            case 0:
+                this.fixed_brush_angle = 45;
+                break;
+            case 45:
+                this.fixed_brush_angle = 0;
+                break;
+            case 90:
+                this.fixed_brush_angle = 135;
+                break;
+            case 135:
+                this.fixed_brush_angle = 90;
+                break;
         }
+    }
 
-	update_color(color) {
-                this.color = color;
-	}
+    /**
+     * Updates the color of the layer.
+     * @param {Array} color - The new color of the layer.
+     */
+    update_color(color) {
+        this.color = color;
+    }
 
-        set_vector_canvas(canvas){
-                this.vector_canvas = canvas;
-        }
-        get_vector_canvas(){
-                return this.vector_canvas;
-        }
+    /**
+     * Sets the vector canvas of the layer.
+     * @param {Object} canvas - The new vector canvas of the layer.
+     */
+    set_vector_canvas(canvas){
+        this.vector_canvas = canvas;
+    }
 
+    /**
+     * Gets the vector canvas of the layer.
+     * @return {Object} The vector canvas of the layer.
+     */
+    get_vector_canvas(){
+        return this.vector_canvas;
+    }
 }
